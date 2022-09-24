984a654b62f3cb79a8d7b3f91ca1385ed062bb49
Vikas Chauhan <vikas@rockstarchef.in>
2022-09-24 14:52:17 +0530

    learn classes

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
-// Factory function
-function product(itemName, price, discount, itemCode) {
-  return {
-    itemName: itemName,
-    price: price,
-    discount: discount,
-    itemCode: itemCode
+class Product {
+  constructor(name, price, discount, barcode) {
+    this.name = name;
+    this.price = price;
+    this.discount = discount;
+    this.barcode = barcode;
   }
 }
 
-const football = product('football', 800, 10, 'F776688');
+const pencil = new Product("Pencil", 3, 0.1, 'LJL;J9F9F9V');
+console.log(pencil);
 
-// Constructor function
+const Product1 = class Product2 {
+  constructor(name, price, discount, barcode) {
+    this.name = name;
+    this.price = price;
+    this.discount = discount;
+    this.barcode = barcode;
+  }
+  getPrice() { return this.price; }
+  set setPrice(value) { this.price = value; }
+  get getDiscount() { return this.price * (this.discount / 100); }
+}
 
-function Product1(itemName, price, discount, itemCode) {
-  this.itemName = itemName;
-  this.price = price;
-  this.discount = discount;
-  this.itemCode = itemCode;
-  this.getDiscount = function () {
-    return this.price * (this.discount / 100);
+const mobile = new Product1('Moto', 1300, 5, 'KAWEFKJWFJL');
+console.log(mobile);
+console.log(Product1.name);
+console.log(mobile.getPrice());
+mobile.setPrice = 1500;
+console.log(mobile.getPrice());
+console.log(mobile.getDiscount);
+
+
+
+class WoodProduct {
+  constructor(name) {
+    console.log("Passed by WoodProduct");
+    this.name = name;
+  }
+  get getName() {
+    return this.name + " is a WoodProduct";
+  }
+}
+class Furniture extends WoodProduct {
+  constructor(name) {
+    super(name);
+  }
+  get getName() {
+    return this.name + " is a Furniture";
   }
 }
 
-const mobile = new Product1('Moto G52', 1400, 9, 'HL9kN0NNK');
-console.log(mobile.getDiscount()) 
+const duster = new WoodProduct("Pencil");
+const chair = new Furniture("Chair");
+
+console.log(duster.getName);
+console.log(chair.getName);
